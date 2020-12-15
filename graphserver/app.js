a9058873d523094ef580adad14f2e3a1a0a15cdb
Devesh <rockdev1234560@gmail.com>
2020-12-15 12:48:40 +0530

    hey

--- graphserver/app.js
+++ graphserver/app.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const {graphqlHTTP} = require('express-graphql');
+const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 //helps express understand graphql
 //also used as middleware
 
 const app = express();
 
+mongodb+srv://Dev:<6pbj8ThNKTBsOupS>@cluster0.op0lq.mongodb.net/<dbname>?retryWrites=true&w=majority
+
+
 //used as middleware here
 app.use('/graphql',graphqlHTTP({
-
+    //schema:schema  ES6 syntax below used
+    schema,
+    //providing a string
+    graphiql:true
 }));
 //the above function graphqlHTTP take some options inside an object
 //the above functio which uses the middleware.. it needs to know about the graph
--- graphserver/schema/schema.js
+++ graphserver/schema/schema.js
@@ -1,15 +1,68 @@
 const graphql = require('graphql');
+const _ = require('lodash');
 
-const {GraphQLObjectType,GraphQLString} = graphql;
+const {GraphQLObjectType,GraphQLString,GraphQLID,GraphQLInt,GraphQLList} = graphql;
+
+// dummy data
+//BELOW
+//used author id to associate each book with its author
+var books = [
+    { name: 'Name of the Wind', genre: 'Fantasy', id: '1',authorId:'1' },
+    { name: 'The Final Empire', genre: 'Fantasy', id: '2',authorId:'2' },
+    { name: 'The Hero of Ages', genre: 'Fantasy', id: '4', authorId: '2' },
+    { name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3' },
+    { name: 'The Colour of Magic', genre: 'Fantasy', id: '5', authorId: '3' },
+    { name: 'The Light Fantastic', genre: 'Fantasy', id: '6', authorId: '3' },
+];
+
+var authors = [
+    {name:"patrick Rothfuse", age:44,id:'1'},
+    {name:"pat", age:42,id:'2'},
+    {name:"patrick ", age:64,id:'3'}
+];
+
 
 //defined our object type
 const BookType = new GraphQLObjectType({
     name:'Book',
+    //put functions so that we do not execute it until all of it has run
+    //function keeps it running untik completion
     fields:()=>({
       //important to be wrapped in a function
-      id: {type: GraphQLString },
+      //it has different feilds
+      id: {type: GraphQLID },
       name: {type: GraphQLString},
-      genre: {type: GraphQLString}  
+      genre: {type: GraphQLString},
+      author:{
+          type:AuthorType,
+          //resolve to get data from a place
+          resolve(parent,args){
+            return _.find(authors, { id: parent.authorId});
+          }
+      }  
+    })
+});
+
+const AuthorType = new GraphQLObjectType({
+    name:'Author',
+    //put functions so that we do not execute it until all of it has run
+    //function keeps it running untik completion
+    fields:()=>({
+      //important to be wrapped in a function
+      //it has different feilds
+      id: {type: GraphQLID},
+      name: {type: GraphQLString},
+      age: {type: GraphQLInt},
+      books: {
+          //graphQl List of Book Types
+          type: new GraphQLList(BookType),
+          resolve(parent,args){
+              return _.filter(books, {authorId: parent.id});
+              //all the objects matching the above can remain
+              //rest are filtered
+              //#lodash
+          }
+      }
     })
 });
 
@@ -20,13 +73,23 @@ const RootQuery = new GraphQLObjectType({
        book: {
            type: BookType,
            //pass down args to know which item (here book) is needed
-           args: {id:{type: GraphQLString}},
+           args: {id:{type: GraphQLID}},
            //resolve function to get data we need from DB or other source
            resolve(parent,args){
                 //parents will come into play during data relationships
-                
+               return _.find(books,{id: args.id});
             }
-       } 
+       }, 
+       author: {
+        type: AuthorType,
+        //pass down args to know which item (here book) is needed
+        args: {id:{type: GraphQLID}},
+        //resolve function to get data we need from DB or other source
+        resolve(parent,args){
+             //parents will come into play during data relationships
+            return _.find(authors,{id: args.id});
+         }
+    }
     }
 });
 
