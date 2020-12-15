45e5290d8aa35f573445aab8f845fde43c4a7994
Devesh <rockdev1234560@gmail.com>
2020-12-16 02:39:45 +0530

    react laid

--- graphserver/db/connect.js
+++ graphserver/db/connect.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
+//using dotenv to manage ENV variables and use process.env
+//the above reads the environment variable files and saves them
 
 module.exports = async function(){
 	try{
@@ -10,3 +12,6 @@ module.exports = async function(){
 		process.exit(0);
 	}
 }
+
+/*The process.env global variable is injected by the Node at runtime for your application to use and it represents the 
+state of the system environment your application is in when it starts.*/ 
